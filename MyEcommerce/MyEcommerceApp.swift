d2ac83049d34c92bb1b0d00fb2052b0ca8a6583e
Brahim Lamri <brahim.llmr@gmail.com>
2024-10-10 19:59:03 +0200

    Initial Commit

---
+++ MyEcommerce/MyEcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyEcommerceApp.swift
+//  MyEcommerce
+//
+//  Created by Brahim Lamri on 10/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyEcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyEcommerceTests/MyEcommerceTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyEcommerceTests.swift
+//  MyEcommerceTests
+//
+//  Created by Brahim Lamri on 10/10/2024.
+//
+
+import Testing
+@testable import MyEcommerce
+
+struct MyEcommerceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
